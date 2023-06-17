f0f84a6d223c23c7859d0e4cc5265c9e4c9fe03c
Kim Yeo Jin <74007879+Warummm@users.noreply.github.com>
2023-06-17 21:48:59 +0900

    Time: 121 ms (66.00%), Space: 67.8 MB (41.97%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
+    int maxSubArray(vector<int>& nums) { // Kadane's Algorithm
         int current_sum = 0;
         int max_sum = INT_MIN;
-        for (int i: nums) {
-            current_sum = max(i, current_sum + i);
+        for (int i: nums) { 
+            current_sum = max(i, current_sum + i); 
             max_sum = max(max_sum, current_sum);
         }
         return max_sum;
@@ -17,7 +17,7 @@ int maxSubArray(vector<int>& nums) {
         sum[0] = nums[0];
 
         for (int i=1; i<n; i++) {
-            if (sum[i-1] <= 0)
+            if (sum[i-1] <= 0) // dont re-use negative value. (Because goal is the maximum->dont care)
                 sum[i] = nums[i];
             else
                 sum[i] = sum[i-1] + nums[i];
